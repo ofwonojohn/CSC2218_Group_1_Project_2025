ed69ac4203fee7dad79804346a5e78683b9473a6
Ofwono john <ofwonojohn004@gmail.com>
2025-04-09 18:34:51 +0300

    worked on domain layer

--- domain/entities/account.py
+++ domain/entities/account.py
@@ -1,12 +1,16 @@
 from typing import List
 from domain.entities.transaction import Transaction
 from uuid import uuid4
+import datetime
 
 class Account:
-    def __init__(self, owner_name: str, initial_balance: float = 0.0):
+    def __init__(self, owner_name: str, account_type: str = "checking", initial_balance: float = 0.0):
         self.account_id = str(uuid4())  # Generate unique ID
         self.owner_name = owner_name
+        self.account_type = account_type  
+        self.status = "active"  
         self.balance = initial_balance
+        self.creation_date = datetime.datetime.now()  
         self.transactions: List[Transaction] = []
 
     def deposit(self, amount: float):
