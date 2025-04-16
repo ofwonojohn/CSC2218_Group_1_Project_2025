5d8d4cc8cd0bf7242eb7f0366fa65b3d7e797042
Ofwono john <ofwonojohn004@gmail.com>
2025-04-16 09:37:10 +0300

    included the account status and type objects in the Account class

--- domain/entities/account.py
+++ domain/entities/account.py
@@ -1,16 +1,28 @@
 from typing import List
-from domain.entities.transaction import Transaction
 from uuid import uuid4
-import datetime
+from datetime import datetime
+from domain.entities.transaction import Transaction
+from domain.entities.account_type import AccountType
+from domain.entities.account_status import AccountStatus
 
 class Account:
-    def __init__(self, owner_name: str, account_type: str = "checking", initial_balance: float = 0.0):
-        self.account_id = str(uuid4())  # Generate unique ID
+    def __init__(
+        self,
+        owner_name: str,
+        account_type: AccountType = AccountType.CHECKING,
+        balance: float = 0.0,
+        status: AccountStatus = AccountStatus.ACTIVE,
+        account_id: str = None,
+        creation_date: datetime = None,
+        interest_rate: float = 0.0
+    ):
+        self.account_id = account_id or str(uuid4())
         self.owner_name = owner_name
-        self.account_type = account_type  
-        self.status = "active"  
-        self.balance = initial_balance
-        self.creation_date = datetime.datetime.now()  
+        self.account_type = account_type
+        self.status = status
+        self.balance = balance
+        self.creation_date = creation_date or datetime.now()
+        self.interest_rate = interest_rate
         self.transactions: List[Transaction] = []
 
     def deposit(self, amount: float):
