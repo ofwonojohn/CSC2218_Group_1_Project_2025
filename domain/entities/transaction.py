1876386cc7075f4a24429da563b0f1bfba8a19c0
ofwono john <ofwonojohn004@gmail.com>
2025-05-15 18:10:50 +0300

    changed the str to enum field of the respective trasaction types

--- domain/entities/transaction.py
+++ domain/entities/transaction.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 from uuid import uuid4
 from typing import Optional
+from .transaction_type import TransactionType
 
 class Transaction:
     def __init__(
         self,
         account_id: str,
-        transaction_type: str,
+        transaction_type: TransactionType,
         amount: float,
         source_account_id: Optional[str] = None,
         destination_account_id: Optional[str] = None
     ):
         self.transaction_id = str(uuid4())
         self.account_id = account_id
-        self.transaction_type = transaction_type  # e.g., deposit, withdrawal, transfer_in, transfer_out
+        self.transaction_type = transaction_type
         self.amount = amount
         self.timestamp = datetime.now()
         self.source_account_id = source_account_id
         self.destination_account_id = destination_account_id
+
+
